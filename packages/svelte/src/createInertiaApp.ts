cb9e271f1c1a0cd6853d72a4e72dc921b6bbe38b
Stefan Buhrmester <stefan@buhrmi.de>
2026-07-27 12:32:59 +0700

    Revert "fix: seed global page store before render to fix SSR page.props (#11.0.48)"
    This reverts commit 6f0c42ad9a1d84ded4a7e9b2972852b0ced535fc.

--- packages/svelte/src/createInertiaApp.ts
+++ packages/svelte/src/createInertiaApp.ts
@@ -18,7 +18,6 @@ import { hydrate, mount } from 'svelte'
 import App, { type InertiaAppProps } from './components/App.svelte'
 import { setGlobalResolveComponent } from './frameContext.svelte'
 import { config } from './index'
-import { setPage } from './page.svelte'
 import type { ComponentResolver, ResolvedComponent, SvelteInertiaAppConfig } from './types'
 
 type SvelteRenderResult = { body: string; head: string }
@@ -116,11 +115,6 @@ export default async function createInertiaApp<SharedProps extends PageProps = P
   // This is used by the Vite plugin's SSR transform
   if (isServer && !page) {
     return async (page: Page<SharedProps>, render: SvelteServerRender) => {
-      // Seed the global page store before rendering so that child
-      // components reading `import { page } from "inertiax-svelte"`
-      // always see the current page data during SSR.
-      setPage(page)
-
       const initialComponent = (await resolveComponent(page.component, page)) as ResolvedComponent
 
       const props: InertiaAppProps<SharedProps> = {
@@ -158,11 +152,6 @@ export default async function createInertiaApp<SharedProps extends PageProps = P
   }
 
   const initialPage = page || getInitialPageFromDOM<Page<SharedProps>>(id)!
-
-  // Seed the global page store so that `import { page } from "inertiax-svelte"`
-  // has props populated before any component reads it during render.
-  setPage(initialPage)
-
   const serverHeadManager =
     !isServer && serverHead
       ? createHeadManager(
